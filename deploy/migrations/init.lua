0e6396b5d7b6a405738b4bf5ce6f8ebdc2e42d3e
V1Ro-Dev <matveymdmd@mail.ru>
2025-04-06 15:37:12 +0300

    fixed readme + minor lua script fixes

--- deploy/migrations/init.lua
+++ deploy/migrations/init.lua
@@ -1,9 +1,12 @@
+local TARANTOOL_USER = os.getenv("TARANTOOL_USER")
+local TARANTOOL_PASS = os.getenv("TARANTOOL_PASS")
+
 box.cfg{}
 
-if not box.schema.user.exists('test') then
-    box.schema.user.create('test', { password = 'secret' })
+if not box.schema.user.exists(TARANTOOL_USER) then
+    box.schema.user.create('test', { password = TARANTOOL_PASS })
 end
-box.schema.user.grant('test', 'read,write,execute', 'universe')
+box.schema.user.grant(TARANTOOL_USER, 'read,write,execute', 'universe')
 
 local vote = box.schema.space.create('polls', {
     format = {
